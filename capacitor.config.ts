a3af68c2e15fcfdc2750b0e66320e1685ad7318c
annam <amsteffens@lcmail.lcsc.edu>
2024-01-26 13:01:00 -0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Theatre_Auditions_Manager',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
