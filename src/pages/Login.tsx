d69616d49b3c4ca163f90b8f94d41695f72826cb
MSBivens <mikebiv95@gmail.com>
2024-02-16 19:30:18 -0800

    pop up working

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,43 +1,88 @@
-import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar,
-    IonButtons, IonMenu, IonMenuButton } from '@ionic/react';
-import { GoogleLogin } from '@react-oauth/google';
-import './Login.css';
+import React from "react";
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonButtons,
+  IonMenu,
+  IonMenuButton,
+} from "@ionic/react";
+// import { GoogleLogin } from "@react-oauth/google";
+import "./Login.css";
+
+// #################################
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const provider = new GoogleAuthProvider();
+
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
+const auth = getAuth();
+
+signInWithPopup(auth, provider)
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    // The signed-in user info.
+    const user = result.user;
+    // IdP data available using getAdditionalUserInfo(result)
+    // ...
+  })
+  .catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+  });
+
+//   #########################
 
 const Login: React.FC = () => {
+  const handleSuccess = (credentialResponse) => {
+    console.log(credentialResponse);
+  };
 
-    const handleSuccess = (credentialResponse) => {
-        console.log(credentialResponse);
-    };
+  const handleError = () => {
+    console.log("Login Failed");
+  };
 
-    const handleError = () => {
-        console.log('Login Failed');
-    };
+  return (
+    <>
+      <IonPage>
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>Login with Google</IonTitle>
+          </IonToolbar>
+        </IonHeader>
 
-    return (
-        <>
-        <IonPage>
-            <IonHeader>
-                <IonToolbar>
-                    <IonTitle>Login with Google</IonTitle>
-                </IonToolbar>
-            </IonHeader>
+        <IonContent fullscreen>
+          <h1>Testing</h1>
 
-            <IonContent fullscreen>
-                <h1>Testing</h1>
-
-            <div>
-                <GoogleLogin
-                    onSuccess={handleSuccess}
-                    onError={handleError}
-
-                />
-            </div>
-
-            </IonContent>
-        </IonPage>
-        </>
-    );
- };
+          <div>
+            {/* <GoogleLogin onSuccess={handleSuccess} onError={handleError} /> */}
+          </div>
+        </IonContent>
+      </IonPage>
+    </>
+  );
+};
 
 export default Login;
