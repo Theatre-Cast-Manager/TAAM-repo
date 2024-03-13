c6240de35a55bc1bd02716885c782158fc9c1d71
Jordan B <jabader@lcmail.lcsc.edu>
2024-03-12 18:38:13 -0600

    Redoing routing and login/logout
    Renamed pages, updated routing, removed 'go to dashboard' and 'logout' buttons from the login page. Automatically redirect users to home when successfully logged in. On home page, side menu has logout button that works.

--- src/App.tsx
+++ src/App.tsx
@@ -16,9 +16,9 @@ import {
 import { IonReactRouter } from "@ionic/react-router";
 import { SplashScreen } from "@capacitor/splash-screen"; // Import SplashScreen
 import { ellipse, square, triangle, folder, images } from "ionicons/icons";
-import DashboardPage from "./pages/dashboard";
-// import Tab2 from "./pages/Tab2";
-import Login from "./pages/Login";
+import HomePage from "./pages/home";
+import Tab2 from "./pages/Tab2";
+import LoginPage from "./pages/Login";
 
 /* Core CSS required for Ionic components to work properly */
 import "@ionic/react/css/core.css";
@@ -49,7 +49,6 @@ const App: React.FC = () => {
     SplashScreen.hide(); // Hide the splash screen once the app is ready
   }, []);
 
-
 /*%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%                            Component Structure and Routing                                   %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%*/
@@ -57,9 +56,10 @@ const App: React.FC = () => {
     <IonApp>
       <IonReactRouter>
         <IonRouterOutlet>
-          {/* Routs: define different pages of application and their corresponding URL paths. */} 
-          <Route path="/dashboard" component={DashboardPage} />
-          <Route path="/login" component={Login} />
+          {/* Routes: define different pages of application and their corresponding URL paths. */} 
+          <Route path="/home" component={HomePage} />
+          <Route path="/login" component={LoginPage} />
+          <Route path="/tab2" component={Tab2} />
           {/* Redirects */}
           <Redirect exact from="/" to="/login" />
         </IonRouterOutlet>
