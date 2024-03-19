8bed0e0d8f8ef57b290c62b2a09f90cd640e1ec0
Anestasha <anna.mcs.2002@gmail.com>
2024-03-19 13:47:28 -0700

    Bundle Viewing Functions
    Created page components to direct organizers and collect sheet URLs.
    
    Added functions to handle user input and submission.
    
    Created variables to hold sheet data and information about the data.
    
    Created functions to parse fetched data and variables to hold essential information.
    
    Added functions to properly display an image that was uploaded to a google form.
    
    Added functions for quick expansion and collapsing of all summary and detail elements on a page.

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -10,13 +10,16 @@ import {
   IonButtons,
   IonMenu,
   IonMenuButton,
+  IonInput,
 } from "@ionic/react";
-import ExploreContainer from "../components/ExploreContainer";
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import { handleLogout } from "../authService";
 import "./home.css";
 
 const HomePage: React.FC = () => {
+  /*%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  %%                                        Login functionality                                   %%
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%*/
   const [userName, setUserName] = useState<string | null>(null); // To store user's name
   const [userPhotoUrl, setUserPhotoUrl] = useState<string | null>(null);
   const [errorMessage, setErrorMessage] = useState<string>('');
@@ -50,6 +53,147 @@ const HomePage: React.FC = () => {
       });
   };
 
+  /*%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  %%                                  Data Gathering Functionality                                %%
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%*/
+  //A reminder of what our input element looks like:
+  // <IonInput label='sheet_url' value={givenUrl} ref={ionInputEl} onIonInput={handleInput}></IonInput>
+  // the value attribute controls what is displayed in the input field - we will clear this after the submit URL button has been clicked to clear the field
+  // the ref attribute allows us to connect our element to a reference we can use to manipulate the field and gather information from it
+  
+  //Controls our input element (this is what we assign 'ref' in the input element)
+  const ionInputEl = useRef<HTMLIonInputElement>(null);
+
+  //Holds the URL given to us by the user
+  const [givenUrl, setGivenUrl] = useState("");
+
+  //Updates the URL given to us when the value in the input element is changed
+  const handleInput = (e: CustomEvent) => {
+    setGivenUrl(e.detail.value as string);
+  };
+
+  //Holds the sheetID we will feed to our API call
+  const [sheetID, setSheetID] = useState("");
+
+  //Sets our sheetID variable when the user submits a URL
+  const handleClick = ( e : Event ) => {
+    //Access the given URL indirectly (indirect because it is set in the handleInput() function)
+    const value = givenUrl;
+    const startIndex = value.indexOf("/d/") + 3;
+    const endIndex = value.indexOf("/edit");
+
+    //Extract the sheet ID from the givenURL and update our sheetID variable
+    if (startIndex >= 0) {
+      setSheetID(value.substring(startIndex, endIndex));
+    } 
+
+    //Clear the input field
+    setGivenUrl("");
+
+    //TODO - error handling
+  }
+
+  //Holds the form field information and the fetched data
+  const [data, setData] = useState<any>(null);
+  const [formFields, setFormFields] = useState([]);
+
+
+  //TODO - API call to fetch data
+  //Something like...
+  // useEffect( () => {
+  //   const fetchData = async () => {
+  //   if(sheetID){
+  //     try{
+  //       //Here is where the API call would go
+  //       //If our call is successful we would set our 'data' variable and our 'formFields' value to be the first row from the 'data' array
+  //     }
+  //     catch(err){
+  //       console.error("Error in fetching data: ", err);
+  //     }
+  //   }
+  //  };
+  //  fetchData();
+  // }, [sheetID]) //sheetID is only updated after the submit button is clicked, so we can add it as a dependency.
+
+  /*%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  %%                                  Data Parsing Functionality                                  %%
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%*/
+
+  //Holds information about our data that we need to properly display it
+  const [urlColumn, setUrlColumn] = useState(-1);
+  const [nullCols, setNullCols] = useState<number[]>([]);
+  const [nameCol, setNameCol] = useState(-1);
+
+  //Data parsing function
+  useEffect( () => {
+    const parseData = () => {
+      if(data){
+        formFields.forEach((columnData, columnIndex) => {
+          //Look for the name column
+          if(String(columnData).includes("name")){ //might need to include more than one check here for thourough-ness
+            setNameCol(columnIndex);
+          }
+          //Check for any null columns we may need to skip over later
+          if(!columnData){
+            setNullCols(prevNullCols => [...prevNullCols, Number(columnIndex)]);
+          }
+          //Determine which column holds the image url
+          let firstRow = data[1];
+          firstRow.forEach((columnData, columnIndex) => {
+            if(String(columnData).startsWith('http')){
+              setUrlColumn(columnIndex);
+            }
+          })
+        })
+      }
+      else{
+        console.log("data is null");
+      }
+    }
+    parseData();
+  })
+
+  /*%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  %%                                  Image Display Functionality                                 %%
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%*/
+  //TODO: update alt tag with actor's name
+  //Example call: <img src={generateThumbnailUrl(extractIdFromUrl(columnData))} alt="Thumbnail" />
+  const extractIdFromUrl = (url) => {
+    const startIndex = url.indexOf("id=") + 3;
+    if (startIndex >= 0) {
+      return url.substring(startIndex);
+    } else {
+      console.error("Invalid URL format");
+      return null; // We may want to update the handling here
+    }
+  };
+  
+  const generateThumbnailUrl = (id) => {
+    //console.log(`https://drive.google.com/thumbnail?sz=w300&id=${id}`);
+    const width = 300;
+    return `https://drive.google.com/thumbnail?sz=w${width}&id=${id}`;
+  };
+
+  /*%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  %%                                 Developer/User Functionality                                 %%
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%*/
+  //These can be attatched to buttons to quickly expand or collapse all summary elements that hold form data
+  const expandAll = () => {
+    const summaries = document.querySelectorAll('summary');
+    summaries.forEach((summary: HTMLSummaryElement) => {
+      const details = summary.parentElement as HTMLDetailsElement;
+      details.open = true;
+    });
+  };
+
+  const collapseAll = () => {
+    const summaries = document.querySelectorAll('summary');
+    summaries.forEach((summary: HTMLSummaryElement) => {
+      const details = summary.parentElement as HTMLDetailsElement;
+      details.open = false;
+    });
+  };
+
   return (
     <>
       <IonMenu contentId="main-content" ref={menuRef}> {/* Attach the ref to IonMenu */}
@@ -78,7 +222,16 @@ const HomePage: React.FC = () => {
               <IonTitle size="large">Holla</IonTitle>
             </IonToolbar>
           </IonHeader>
-          <ExploreContainer name="Tab 1 page" />
+          <div id='instructions'>
+            <p>
+              To view your application submissions, please paste the URL of the sheet associated with your audition form.
+            </p>
+          </div>
+          <div id='url_submission'>
+            <h1>URL:</h1>
+            <IonInput label='sheet_url' value={givenUrl} ref={ionInputEl} onIonInput={handleInput}></IonInput>
+            <IonButton onClick={handleClick}>Submit URL</IonButton>
+          </div>
         </IonContent>
       </IonPage>
     </>
