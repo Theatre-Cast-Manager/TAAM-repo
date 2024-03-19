91316680ca80c90fe6d68960a597c3bc68f3d975
Michael Bivens <mikebiv95@gmail.com>
2024-03-19 12:00:29 -0700

    fixing error

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
-import React, { useEffect, useState } from "react";
+/*%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%                                      Import Statements                                       %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%*/
+import React, { useEffect } from "react";
 import { Redirect, Route } from "react-router-dom";
 import {
   IonApp,
+  IonIcon,
+  IonLabel,
   IonRouterOutlet,
+  IonTabBar,
+  IonTabButton,
+  IonTabs,
   setupIonicReact,
 } from "@ionic/react";
 import { IonReactRouter } from "@ionic/react-router";
-import SplashScreen from './pages/splashscreen';
-//import DashboardPage from "./pages/dashboard";
-import Login from "./pages/Login";
+import { SplashScreen } from "@capacitor/splash-screen"; // Import SplashScreen
+import { ellipse, square, triangle, folder, images } from "ionicons/icons";
+import HomePage from "./pages/home";
+import Tab2 from "./pages/Tab2";
+import LoginPage from "./pages/Login";
 
 /* Core CSS required for Ionic components to work properly */
 import "@ionic/react/css/core.css";
@@ -35,24 +45,21 @@ import "./theme/variables.css";
 setupIonicReact();
 
 const App: React.FC = () => {
-  const [showSplash, setShowSplash] = useState(true);
-
   useEffect(() => {
-    setTimeout(() => {
-      setShowSplash(false);
-    }, 2000); // Hide the splash screen after 2 seconds
+    SplashScreen.hide(); // Hide the splash screen once the app is ready
   }, []);
 
-/*%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  /*%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%                            Component Structure and Routing                                   %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%*/
   return (
     <IonApp>
       <IonReactRouter>
         <IonRouterOutlet>
-          {/* Routs */}
-          {/*<Route path="/dashboard" component={DashboardPage} />*/}
-          <Route path="/login" component={Login} />
+          {/* Routes: define different pages of application and their corresponding URL paths. */}
+          <Route path="/home" component={HomePage} />
+          <Route path="/login" component={LoginPage} />
+          <Route path="/tab2" component={Tab2} />
           {/* Redirects */}
           <Redirect exact from="/" to="/login" />
         </IonRouterOutlet>
