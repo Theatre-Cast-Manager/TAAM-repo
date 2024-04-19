a2912d80a1a4a8bcbc777df51ad82d881b68cabe
Michael Bivens <mikebiv95@gmail.com>
2024-04-18 20:16:18 -0700

    column headers / styling update

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -114,14 +114,13 @@ const HomePage: React.FC = () => {
 
   //Holds the form field information and the fetched data
   const [data, setData] = useState<any>(null);
-  const [formFields, setFormFields] = useState([]);
+  const [formFields, setFormFields] = useState<string[]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
       if (sheetID) {
-        //url for fetching data = urlStart + sheetId + urlEnd = https://sheets.googleapis.com/v4/spreadsheets/[GIVENID]/values/Form Responses 1!A1:L27?key=API_KEY
         const urlStart = "https://sheets.googleapis.com/v4/spreadsheets/";
-        const urlEnd = `/values/Form Responses 1!A1:AA100?key=${googleSheetsApiKey}`; //insert API key here
+        const urlEnd = `/values/Form Responses 1!A1:AA100?key=${googleSheetsApiKey}`;
         const urlComplete = urlStart + sheetID + urlEnd;
 
         try {
@@ -130,15 +129,16 @@ const HomePage: React.FC = () => {
             throw new Error("Failed to fetch data");
           }
           const jsonData = await response.json();
+
+          if (jsonData && jsonData.values && jsonData.values.length > 0) {
+            setFormFields(jsonData.values[0]); // Set formFields with column headers
+          }
+
           setData(jsonData.values);
-          setFormFields(jsonData.values[0]);
 
           const bundleViewer = document.getElementById("form_data");
           if (bundleViewer) {
-            console.log("display");
             bundleViewer.style.display = "block";
-          } else {
-            console.log("hide");
           }
         } catch (err) {
           console.error("Error fetching data: ", err);
@@ -247,30 +247,28 @@ const HomePage: React.FC = () => {
   const styles = StyleSheet.create({
     page: { flexDirection: "column", padding: 10 },
     section: { margin: 5, padding: 5 },
-    text: { margin: 12, fontSize: 14 },
+    text: { fontSize: 12 },
   });
 
-  const SingleAuditionPDF = ({ row }) => {
-    return (
-      <Document>
-        <Page style={styles.page}>
-          {row.map((dataPoint, index) => (
-            <View key={index} style={styles.section}>
-              <Text style={styles.text}>{dataPoint}</Text>
-            </View>
-          ))}
-        </Page>
-      </Document>
-    );
-  };
+  const SingleAuditionPDF = ({ row, formFields }) => (
+    <Document>
+      <Page style={styles.page}>
+        {formFields.map((field, index) => (
+          <View key={index} style={styles.section}>
+            <Text>{`${field}: ${row[index]}`}</Text>
+          </View>
+        ))}
+      </Page>
+    </Document>
+  );
 
-  const AllAuditionsPDF = ({ data }) => (
+  const AllAuditionsPDF = ({ data, formFields }) => (
     <Document>
       {data.map((row, rowIndex) => (
         <Page key={rowIndex} style={styles.page}>
-          {row.map((value, index) => (
+          {formFields.map((field, index) => (
             <View key={index} style={styles.section}>
-              <Text style={styles.text}>{value}</Text>
+              <Text>{`${field}: ${row[index]}`}</Text>
             </View>
           ))}
         </Page>
@@ -279,18 +277,6 @@ const HomePage: React.FC = () => {
   );
 
   return (
-    // <Document>
-    //   {data.slice(1).map((row, rowIndex) => (
-    //     <Page key={rowIndex} style={styles.page}>
-    //       {row.map((value, index) => (
-    //         <View key={index} style={styles.section}>
-    //           <Text>{`${data[0][index]}: ${value}`}</Text>
-    //         </View>
-    //       ))}
-    //     </Page>
-    //   ))}
-    // </Document>
-
     <>
       <IonMenu contentId="main-content" ref={menuRef}>
         <IonHeader>
@@ -356,29 +342,22 @@ const HomePage: React.FC = () => {
                   (row, rowIndex) =>
                     rowIndex > 0 && (
                       <li key={rowIndex}>
+                        {/* Details section to display data with column headers */}
                         <details>
                           <summary>{row[nameCol]}</summary>
-                          {Object.entries(row).map(
-                            ([columnIndex, columnData], index) =>
-                              !nullCols.includes(parseInt(columnIndex)) && (
-                                <div key={index}>
-                                  {parseInt(columnIndex) === urlColumn ? (
-                                    <img
-                                      src={generateThumbnailUrl(
-                                        extractIdFromUrl(columnData)
-                                      )}
-                                      alt="Thumbnail"
-                                    />
-                                  ) : (
-                                    <p>{`${
-                                      formFields[parseInt(columnIndex)]
-                                    }: ${columnData}`}</p>
-                                  )}
-                                </div>
-                              )
-                          )}
+                          {formFields.map((field, index) => (
+                            <div key={index}>
+                              <p>{`${field}: ${row[index]}`}</p>
+                            </div>
+                          ))}
+                          {/* Export to PDF link */}
                           <PDFDownloadLink
-                            document={<SingleAuditionPDF row={row} />}
+                            document={
+                              <SingleAuditionPDF
+                                row={row}
+                                formFields={formFields}
+                              />
+                            }
                             fileName={`${row[nameCol]}-audition.pdf`}
                           >
                             {({ loading }) =>
