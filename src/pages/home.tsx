9ee46b298321d550144d69fec969a14b95b493ef
Michael Bivens <mikebiv95@gmail.com>
2024-04-18 19:50:51 -0700

    removing old code

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -30,7 +30,7 @@ import {
   StyleSheet,
 } from "@react-pdf/renderer";
 const googleSheetsApiKey = import.meta.env.VITE_GOOGLE_SHEETS_API_KEY;
-  //this ^ key is a generic API key for our entire project, not just Google Sheets. See Google Cloud Console
+//this ^ key is a generic API key for our entire project, not just Google Sheets. See Google Cloud Console
 
 /*%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%                                        'Home' page Typescript                                %%
@@ -305,7 +305,9 @@ const HomePage: React.FC = () => {
               alt="User Profile"
             />
             <h1 id="userName">{userName}</h1>
-            <IonButton id="logoutButton" onClick={logout}>Logout</IonButton>
+            <IonButton id="logoutButton" onClick={logout}>
+              Logout
+            </IonButton>
           </div>
         </IonContent>
       </IonMenu>
@@ -334,14 +336,20 @@ const HomePage: React.FC = () => {
               ref={ionInputEl}
               onIonInput={handleInput}
             ></IonInput>
-            <IonButton id="auditionsButton" onClick={handleClick}>View Auditions</IonButton>
+            <IonButton id="auditionsButton" onClick={handleClick}>
+              View Auditions
+            </IonButton>
           </div>
 
           <div id="form_data_summary">
             <h2>Auditions:</h2>
             <p> Select a name to view a single audition</p>
-            <IonButton id="expandButton" onClick={expandAll}>Expand All</IonButton>
-            <IonButton id="collapseButton" onClick={collapseAll}>Collapse All</IonButton>
+            <IonButton id="expandButton" onClick={expandAll}>
+              Expand All
+            </IonButton>
+            <IonButton id="collapseButton" onClick={collapseAll}>
+              Collapse All
+            </IonButton>
             {data && (
               <ul>
                 {data.map(
@@ -411,13 +419,6 @@ const HomePage: React.FC = () => {
                     }
                   </PDFDownloadLink>
                 )}
-
-                {/* <PDFDownloadLink
-            document={<AllAuditionsPDF data={data} />}
-            fileName="all-auditions.pdf"
-            >
-            {({ loading }) => (loading ? 'Loading document...' : 'Export All Auditions')}
-            </PDFDownloadLink> */}
               </div>
             </div>
           </IonToolbar>
@@ -426,127 +427,5 @@ const HomePage: React.FC = () => {
     </>
   );
 };
-
-/*%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%                     'home' page HTML from dev branch as of 04/05/2024                        %%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%*/
-// return (
-//     <>
-//       <IonMenu contentId="main-content" ref={menuRef}>
-//         {" "}
-//         {/* Attach the ref to IonMenu */}
-//         <IonHeader>
-//           <IonToolbar>
-//             <IonTitle id="yourAccount">Your Account</IonTitle>
-//           </IonToolbar>
-//         </IonHeader>
-//         <IonContent className="ion-padding">
-//           <div className="profile">
-//             <img
-//               src={userPhotoUrl || "../../public/test_logo.jpg"}
-//               alt="User Profile"
-//             />
-//             <h1 id="userName">{userName}</h1> {/* Display the user's name */}
-//             <IonButton id="logoutButton" onClick={logout}>Logout</IonButton>
-//           </div>
-//         </IonContent>
-//       </IonMenu>
-
-//       <IonPage id="main-content">
-//         <IonButtons slot="start">
-//           <IonMenuButton></IonMenuButton>
-//         </IonButtons>
-
-//         <IonContent fullscreen>
-//           <IonHeader collapse="condense">
-//             <IonToolbar>
-//               <IonTitle size="large">Holla</IonTitle>
-//             </IonToolbar>
-//           </IonHeader>
-//           <div id="instructions">
-//             <p>
-//               To load audition data, paste the URL of the Sheet associated with
-//               your audition Form, and click 'View Auditions'
-//             </p>
-//           </div>
-//           <div id="url_submission">
-//             <IonInput
-//               label="URL:"
-//               value={givenUrl}
-//               ref={ionInputEl}
-//               onIonInput={handleInput}
-//             ></IonInput>
-
-//             <IonButton id="auditionsButton"onClick={handleClick}>View Auditions</IonButton>
-//           </div>
-
-//           <div id="form_data_summary">
-//             <h2>Auditions:</h2>
-//             <p> Select a name to view a single audition</p>
-//             <IonButton id="expandButton"onClick={expandAll}>Expand All</IonButton>
-//             <IonButton id="collapseButton"onClick={collapseAll}>Collapse All</IonButton>
-//             {data && (
-//               <ul>
-//                 {data.map(
-//                   (row: any, rowIndex: number) =>
-//                     rowIndex > 0 && (
-//                       // Summary for each form
-//                       <li key={rowIndex}>
-//                         <details>
-//                           <summary>{data[rowIndex][nameCol]}</summary>
-//                           <ul>
-//                             {Object.entries(row).map(
-//                               ([columnName, columnData], cellIndex: number) =>
-//                                 // Summary for each field in the form IF it's not null (MISSING)
-//                                 !nullCols.includes(parseInt(columnName)) && (
-//                                   <li key={columnName}>
-//                                     {parseInt(columnName) === urlColumn ? (
-//                                       <img
-//                                         src={generateThumbnailUrl(
-//                                           extractIdFromUrl(columnData)
-//                                         )}
-//                                         alt="Thumbnail"
-//                                       />
-//                                     ) : (
-//                                       <>
-//                                         <strong>
-//                                           {formFields[parseInt(columnName)]}:{" "}
-//                                           <br />{" "}
-//                                         </strong>
-//                                         {columnData}
-//                                       </>
-//                                     )}
-//                                   </li>
-//                                 )
-//                             )}
-//                           </ul>
-//                         </details>
-//                       </li>
-//                     )
-//                 )}
-//               </ul>
-//             )}
-//           </div>
-//         </IonContent>
-//         <IonFooter>
-//           <IonToolbar>
-//             <div>
-//               <p>
-//                 {" "}
-//                 Select 'Export All' to export all auditions as a single .pdf
-//                 files
-//               </p>
-//               {/* <p> Select 'Clear Data' to clear all data</p> */}
-//               <div>
-
-//                 <IonButton id="exportButton">Export All</IonButton>
-//                 {/* <IonButton>Clear Data</IonButton> */}
-//               </div>
-//             </div>
-//           </IonToolbar>
-//         </IonFooter>
-//       </IonPage>
-//     </>
-//   );
 
 export default HomePage;
